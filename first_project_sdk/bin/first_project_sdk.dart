1ebd3c08caa0618d1de00149993728e245287933
BSDK1311bsdk <basimqwaysim@yahoo.com>
2023-10-03 17:48:34 +0300

    staart new dart project
    constants

---
+++ first_project_sdk/bin/first_project_sdk.dart
@@ -0,0 +1,20 @@
+
+void main(){
+  //we will learn about data types in dart
+  //numbers( int, double) , string, bool, list, map
+  //int: all whole numbers
+  int age1=20;
+  var age2=30;
+  const age3=40;
+  final age4=50;
+  double grade=85.2;
+  String name="basim";
+  print(age1);
+  print(age2);
+  print(age3);
+  print(age4);
+  print(grade);
+  print(name);
+
+  
+}
---
+++ first_project_sdk/test/first_project_sdk_test.dart
@@ -0,0 +1,8 @@
+import 'package:first_project_sdk/first_project_sdk.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
